25acebcecdb8e3789c784dc5442ecc0a527a8c7c
Andrew Druffner <andrew@nomstock.com>
2019-12-31 20:58:00 -0800

    added preliminary readme

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
-var wpdir = require('wporg-mini')
+const wpOrg = require('wporg-mini')
 
 const options = {
     apiDelay: 2000,
@@ -10,8 +10,8 @@ const options = {
 
 //var RequestPromise = require("request-promise");
 
-const wpThemes = new wpdir(options, "themes");
-const wpPlugins = new wpdir(options, "plugins");
+const wpThemes = new wpOrg(options, "themes");
+const wpPlugins = new wpOrg(options, "plugins");
 function printOutItem(item, pageNumber, itemNumber) {
     console.log('PageNumber: ' + pageNumber + ' Item Number: ' + itemNumber + ' ' + item["name"])
     //console.log(item["name"])
